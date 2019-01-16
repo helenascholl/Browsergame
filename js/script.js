012b96fd40e488ea2d4f0c32cde83bcac0f6c7be
Sebastian Scholl <scholl.sebastian@gmx.at>
2019-01-16 17:42:03 +0100

    improved

--- js/script.js
+++ js/script.js
@@ -4,20 +4,55 @@ const MIN_Y = 100;
 const MAX_X = window.innerWidth - 100;
 const MAX_Y = window.innerHeight - 100;
 const VALID_KEYS = ['w', 'a', 's', 'd', 'W', 'A', 'S', 'D', ' ', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+let player;
+let body;
 let pressedKeys = [];
 let bulletIntervals = [];
 let reloaded = true;
 let bulletCounter = 0;
 
 window.addEventListener('load', () => {
-    let character = document.getElementById('character');
-    let body = document.getElementById('body');
+    let div = document.createElement('div');
 
-    character.direction = 'right';
-    character.velocity = 10;
-    character.style.top = MIN_Y + 'px';
-    character.style.left = MIN_X + 'px';
-    character.style.display = 'block';
+    body = document.getElementById('body');
+
+    div.style.display = 'none';
+    for (let i = 0; i <= 8; i++) {
+        let player_up = document.createElement('img');
+        let player_left = document.createElement('img');
+        let player_down = document.createElement('img');
+        let player_right = document.createElement('img');
+        let enemy_up = document.createElement('img');
+        let enemy_left = document.createElement('img');
+        let enemy_down = document.createElement('img');
+        let enemy_right = document.createElement('img');
+
+        player_up.src = './img/player/up' + i + '.png';
+        player_left.src = './img/player/left' + i + '.png';
+        player_down.src = './img/player/down' + i + '.png';
+        player_right.src = './img/player/right' + i + '.png';
+        enemy_up.src = './img/enemy/up' + i + '.png';
+        enemy_left.src = './img/enemy/left' + i + '.png';
+        enemy_down.src = './img/enemy/down' + i + '.png';
+        enemy_right.src = './img/enemy/right' + i + '.png';
+
+        div.appendChild(player_up);
+        div.appendChild(player_left);
+        div.appendChild(player_down);
+        div.appendChild(player_right);
+        div.appendChild(enemy_up);
+        div.appendChild(enemy_left);
+        div.appendChild(enemy_down);
+        div.appendChild(enemy_right);
+    }
+    body.appendChild(div);
+
+    player = document.getElementById('player');
+    player.src = './img/player/right0.png'
+    player.direction = 'right';
+    player.velocity = 10;
+    player.style.top = MIN_Y + 'px';
+    player.style.left = MIN_X + 'px';
 
     for (let child of body.childNodes) {
         if (child.data == '\n') {
@@ -42,39 +77,38 @@ window.addEventListener('keydown', (element) => {
 });
 
 function interval() {
+    animateCharacters();
     interpretKeys();
     moveBullets();
     moveEnemies();
 }
 
 function interpretKeys() {
-    let character = document.getElementById('character');
-
     if (pressedKeys['w'] || pressedKeys['W'] || pressedKeys['ArrowUp']) {
-        character.direction = 'up';
-        move(character);
+        player.direction = 'up';
+        move(player);
     }
     if (pressedKeys['a'] || pressedKeys['A'] || pressedKeys['ArrowLeft']) {
-        character.direction = 'left';
-        move(character);
+        player.direction = 'left';
+        move(player);
     }
     if (pressedKeys['s'] || pressedKeys['S'] || pressedKeys['ArrowDown']) {
-        character.direction = 'down';
-        move(character);
+        player.direction = 'down';
+        move(player);
     }
     if (pressedKeys['d'] || pressedKeys['D'] || pressedKeys['ArrowRight']) {
-        character.direction = 'right';
-        move(character);
+        player.direction = 'right';
+        move(player);
     }
     if (pressedKeys[' '] && reloaded) {
-        shoot(character);
+        shoot(player);
         reloaded = false;
         setTimeout(reload, 300);
     }
 }
 
 function move(character) {
-    let top = character.style.top;
+    let top = player.style.top;
     let left = character.style.left;
     
     switch (character.direction) {
@@ -115,17 +149,15 @@ function move(character) {
 function shoot() {
     let longSide = '20px';
     let shortSide = '6px';
-    let character = document.getElementById('character');
-    let body = document.getElementById('body');
     let bullet = document.createElement('div');
 
     bullet.className = 'bullet';
-    bullet.direction = character.direction;
+    bullet.direction = player.direction;
     bullet.number = bulletCounter;
     bullet.style.backgroundColor = 'black';
     bullet.style.position = 'absolute';
-    bullet.style.top = (parseInt(character.style.top) + parseInt(character.height) / 2) + 'px';
-    bullet.style.left = (parseInt(character.style.left) + parseInt(character.width) / 2) + 'px';
+    bullet.style.top = (parseInt(player.style.top) + parseInt(player.height) / 2) + 'px';
+    bullet.style.left = (parseInt(player.style.left) + parseInt(player.width) / 2) + 'px';
 
     if (bullet.direction == 'up' || bullet.direction == 'down') {
         bullet.style.height = longSide;
@@ -169,7 +201,7 @@ function moveBullets() {
         if (left < MIN_X || left > MAX_X || top < MIN_Y || top > MAX_Y) {
             clearInterval(bulletIntervals['bullet' + bullet.number]);
             bulletIntervals.splice('bullet' + bullet.numer);
-            document.getElementById('body').removeChild(bullet);
+            body.removeChild(bullet);
         }
     }
 }
@@ -180,13 +212,12 @@ function reload() {
 
 function spawnEnemy() {
     let enemy = document.createElement('img');
-    let character = document.getElementById('character');
     let isTooClose = false;
 
     enemy.src = './img/enemy.png';
     enemy.alt = 'enemy';
-    enemy.height = character.height;
-    enemy.width = character.width;
+    enemy.height = player.height;
+    enemy.width = player.width;
     enemy.className = 'enemy';
     enemy.direction = 'left';
     enemy.velocity = 3;
@@ -199,8 +230,8 @@ function spawnEnemy() {
         enemy.style.top = (parseInt(Math.random() * (MAX_Y - MIN_Y)) + MIN_Y) + 'px';
         enemy.style.left = (parseInt(Math.random() * (MAX_X - MIN_X)) + MIN_X) + 'px';
 
-        distanceX = Math.abs(parseInt(character.style.left) - parseInt(enemy.style.left));
-        distanceY = Math.abs(parseInt(character.style.top) - parseInt(enemy.style.top));
+        distanceX = Math.abs(parseInt(player.style.left) - parseInt(enemy.style.left));
+        distanceY = Math.abs(parseInt(player.style.top) - parseInt(enemy.style.top));
 
         if (distanceY < (MAX_Y - MIN_Y) / 4 && distanceX < (MAX_X - MIN_X) / 4) {
             isTooClose = true;
@@ -216,15 +247,13 @@ function spawnEnemy() {
         }
     } while (isTooClose);
 
-    document.getElementById('body').appendChild(enemy);
+    body.appendChild(enemy);
 }
 
 function moveEnemies() {
-    let character = document.getElementById('character');
-
     for (let enemy of document.getElementsByClassName('enemy')) {
-        let distanceX = parseInt(character.style.left) - parseInt(enemy.style.left);
-        let distanceY = parseInt(character.style.top) - parseInt(enemy.style.top);
+        let distanceX = parseInt(player.style.left) - parseInt(enemy.style.left);
+        let distanceY = parseInt(player.style.top) - parseInt(enemy.style.top);
 
         if (distanceX > 0 && distanceY > 0) {
             if (distanceX > distanceY) {
@@ -251,5 +280,33 @@ function moveEnemies() {
         }
 
         move(enemy);
+    }
+}
+
+function animateCharacters() {
+    let keyPressed = false;
+
+    for (let key of pressedKeys) {
+        if (key) {
+            key = true;
+        }
+    }
+
+    if (keyPressed) {
+        let imageName = player.src.split('/')[3];
+        let imageDirection = imageName.substring(0, imageName.lenght - 5);
+        let imageNumber = parseInt(imageName.substring(imageName.lenght - 1, imageName.lenght));
+
+        if (imageDirection == player.direction) {
+            if (imageNumber != 8) {
+                player.src = './img/player/' + imageDirection + (imageNumber + 1) + '.png';
+            } else {
+                player.src = './img/player/' + imageDirection + '0.png';
+            }
+        } else {
+            player.src = '.img/player/' + player.direction + '0.png';
+        }
+    } else {
+        player.src = '.img/player/' + player.direction + '0.png';
     }
 }
